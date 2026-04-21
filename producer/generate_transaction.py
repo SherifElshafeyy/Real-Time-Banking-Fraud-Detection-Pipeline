30b7246295bcf3b0f9f9f6404433001c5ff5182c
Sherif Elshafey <shreefelshafey@gmail.com>
2026-04-22 01:50:55 +0200

    modified code by updating transaction schema and adding kafka producer function

--- producer/generate_transaction.py
+++ producer/generate_transaction.py
@@ -4,42 +4,93 @@ import random
 import time
 import uuid
 from datetime import datetime as dt
-import pycountry
+
+
+
+
 
 
 users = [f"User_{i}" for i in range (1,1001)]
-transaction_type = ["PAYMENT", "WITHDRAWAL", "TRANSFER", "DEPOSIT"]
+transaction_types = ["PAYMENT", "WITHDRAWAL", "TRANSFER", "DEPOSIT"]
 currency_map = {
-    "EGY": "EGP",  # Egypt
+    "EGY": "EGP",  
     "DEU": "EUR",  # Germany
     "FRA": "EUR",  # France
     "GBR": "GBP",  # United Kingdom
-    "ZAF": "ZAR",  # South Africa
-    "NGA": "NGN",  # Nigeria
     "CHN": "CNY",  # China
-    "IND": "INR",  # India
     "ARE": "AED",  # UAE
-    "JPN": "JPY",   # Japan
-    "SAU": "SAR",  # Saudi Arabia
-    "QAT": "QAR",  # Qatar
+    "JPN": "JPY",  
+    "SAU": "SAR",  
+    "QAT": "QAR", 
+    'USA': 'USD'
 }
 def generate_transaction():
-
+    
+    #Countrty Choice
     country = random.choice(list(currency_map.keys()))
+
+    #Amount Choice
+    if random.random() < 0.9:
+        amount=round(random.uniform(1, 5000), 2)   # normal transactions
+                 
+    else:
+        amount = round(random.uniform(5001, 200000), 2)  # rare large ones
+    
+    #Transaction Type Cases
+    transaction_type = random.choice(transaction_types)
+    if transaction_type =='PAYMENT':
+        merchant = random.choice(["Amazon", "Netflix", "Uber", "Apple", "Walmart", "Spotify"])
+    else:
+        merchant = None
 
     return {
         "transaction_id" : str(uuid.uuid4()),
         "user_id" : random.choice(users),
-        "event_time" : dt.now().isoformat(),
-        "transaction_type" : random.choice(transaction_type),
-        "amount": round(random.uniform(0,10000),2),
+        "timestamp" : dt.now().isoformat(),
+        "transaction_type" : transaction_type,
+        "amount": amount,
         "country" : country,
         "currency" : currency_map[country],
+        "merchant" : merchant,
         "ip_address": f"192.168.{random.randint(0,255)}.{random.randint(0,255)}",
         "status" : "SUCCESS" if random.random() < 0.95 else "FAILED"
 
     }
 
-    
-print(generate_transaction())
+def validate(err, msg):
+    if err:
+        print(f"❌ Delivery failed: {err}")
+    else:
+        
+        print(f"✅ Delivered to {msg.topic()} : partition {msg.partition()} : at offset {msg.offset()}")
 
+
+
+def transaction_producer():
+    producer = Producer({
+        "bootstrap.servers": "localhost:9092",
+        "client.id": "transaction-producer"
+    })
+
+    try:
+        while True:
+            transaction = generate_transaction()
+            value = json.dumps(transaction).encode("utf-8")
+
+            producer.produce(
+                key=transaction["user_id"],
+                topic='transactions',
+                value=value,
+                callback=validate
+                
+            )
+
+            producer.poll(0)
+            time.sleep(5)
+            
+
+    except KeyboardInterrupt:
+        print("Stopping producer...")
+        producer.flush()
+
+transaction_producer()
